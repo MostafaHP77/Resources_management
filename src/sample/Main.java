c41e2259b22f1927cc468c35da19d2e093bef40f
Alireza <farshchi78@gmail.com>
2019-01-26 16:55:08 +0330

    4064 add "User search"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,10 +15,7 @@ public class Main extends Application {
 //        primaryStage.setTitle("Hello World");
 //        primaryStage.setScene(new Scene(root, 300, 275));
 //        primaryStage.show();
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
-        fileChooser.showOpenDialog(primaryStage);
-    }
+
 
 
     public static void main(String[] args) {
