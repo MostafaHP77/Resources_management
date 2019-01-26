7b456c0c42709b02f0fca0ee5baba2fac70349e6
Alireza <farshchi78@gmail.com>
2019-01-26 16:55:47 +0330

    4064 add "User search"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +15,7 @@ public class Main extends Application {
 //        primaryStage.setScene(new Scene(root, 300, 275));
 //        primaryStage.show();
 
+    }
 
 
     public static void main(String[] args) {
