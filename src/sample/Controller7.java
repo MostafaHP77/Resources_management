97d73aabdba862a24a0671fedad067b164eaf8b0
Alireza <farshchi78@gmail.com>
2019-01-29 13:51:55 +0330

    4064 complete setLend :)***

--- src/sample/Controller7.java
+++ src/sample/Controller7.java
@@ -1,9 +1,12 @@
 package sample;
 
+import Data.User.Manager;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import java.io.File;
 
 //Lend
 
@@ -23,6 +26,7 @@ public class Controller7 {
     }
 
     public void setLend(){
+        Manager.getInstance().borrow(getUsertype(),getResourcetype(),usercode.getText(),resourcename.getText());
 
     }
 
@@ -34,14 +38,43 @@ public class Controller7 {
     public void setResourcetype(){
         resourcetype.getItems().addAll("Book","Magazine","Thesis","Map","Cd","Historical Document","Historical Picture");
         resourcetype.getItems().clear();
+
+    }
+
+    public File getResourcetype() {
+        String resource =   resourcetype.getValue().toString();
+        switch (resource){
+            case "Book":
+                return new File("book.bin");
+
+            case "Magazine":
+                return new File("magazine.bin");
+            case "Thesis" :
+                return new File("thesis.bin");
+        }
+        return null;
     }
 
     public void setUsertype(){
-        usertype.getItems().addAll("Student","Teacher","Geust");
+        usertype.getItems().addAll("Student","Teacher","Guest");
         usertype.getItems().clear();
     }
 
+    public File getUsertype() {
+        String user = usertype.getValue().toString();
+        switch (user) {
 
+            case "Student":
+                return new File("student.bin");
+
+            case "Teacher":
+                return new File("teacher.bin");
+
+            case "Guest":
+                return new File("guest.bin");
+        }
+        return null;
+        }
 
     public void setList() {
 
